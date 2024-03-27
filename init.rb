dabd4d1a045766afb1745753f5a8571bde165652
choiss7 <choipro@gmail.com>
2024-03-28 00:51:33 +0900

    Update init.rb

--- init.rb
+++ init.rb
@@ -1,12 +1,11 @@
 Redmine::Plugin.register :redmine_gpt_react do
-  name 'Redmine GPT React plugin'
-  author 'Your Name'
-  description 'This is a Redmine plugin using Chat GPT API in SPA'
-  version '0.0.1'
-  url 'http://yourpluginurl.com'
-  author_url 'http://yourauthorurl.com'
+  name 'Redmine GPT Chatbot'
+  author 'choipro'
+  description 'Integrates a GPT-based chatbot into Redmine, offering users an interactive AI assistant within the platform. Built as a SPA for a seamless user experience.'
+  version '1.0.0'
+  url 'https://github.com/choiss7/redmine_gpt_react/'
+  author_url 'https://github.com/choiss7/redmine_gpt_react/'
 
-  permission :redmine_gpt_react, { redmine_gpt_react: [:index] }, public: true
-  menu :top_menu, :redmine_gpt_react, { controller: 'redmine_gpt_react', action: 'index' }, caption: 'Chat GPT'
+  permission :view_redmine_gpt_chatbot, { redmine_gpt_react: [:index] }, public: true
+  menu :top_menu, :redmine_gpt_chatbot, { controller: 'redmine_gpt_react', action: 'index' }, caption: 'GPT Chatbot', if: Proc.new { User.current.logged? }
 end
-
